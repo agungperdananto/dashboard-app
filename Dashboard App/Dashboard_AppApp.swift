ee557104928bb410f4b5e555d4ca46032d8fab20
Agung  Perdananto <agung.perdananto@gmail.com>
2024-05-01 11:34:06 +0700

    Initial Commit

---
+++ Dashboard App/Dashboard_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dashboard_AppApp.swift
+//  Dashboard App
+//
+//  Created by Agung  Perdananto on 01/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Dashboard_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
